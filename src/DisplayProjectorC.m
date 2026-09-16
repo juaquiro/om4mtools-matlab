271f9f757cf417ae33f34c4adcbae5a341f31255
juaquiro <aq@iot.es>
2026-09-16 17:12:44 -0400

    Add docstrings to DisplayProjectorC, DisplayProjectorMatlab, DisplayTypes (GH #3)
    Completes the Display family, converted from Doxygen-style (%>)
    comments to plain MATLAB help text. Comment-only changes, verified
    with check_matlab_code.

--- src/DisplayProjectorC.m
+++ src/DisplayProjectorC.m
@@ -1,47 +1,33 @@
-%> @file DisplayProjectorC.m
-%> @brief C++ wrapper image projector class
-%> @copyright 2019 IOT
-%> @author SS 09/19
-
-% ======================================================================
-%> @brief this class implements the DisplayProjector class with c++ dll
-%> @see DisplayTypes for avalible projector follower types
-%> @see DisplayFactory for a static factory
-%> @see testFPADisplayProjectorC for unit tests
-% ======================================================================
 classdef DisplayProjectorC <  handle
-    %% props    
+    % DisplayProjectorC C++ (CProjector DLL) wrapper image projector -
+    % same interface as DisplayProjector, backed by loadlibrary/calllib
+    %
+    % Description:
+    %   See DisplayTypes for available projector types, DisplayFactory
+    %   for a static factory, and testFPADisplayProjectorC for unit tests.
+    %% props
     %public
-    properties  
-        %> dll name
-        Lib;
-        %> c++ .h file
-        LibH;
-        %> cell list of images  
-        gList;
-        %> number of the screen
-        Monitor;
+    properties
+        Lib; % loaded library (DLL) name
+        LibH; % library's .h header file name
+        gList; % cell list of images
+        Monitor; % number of the screen
     end
-            
+
     %get only propos
-    properties (SetAccess=private) 
-        %> screen resolution
-        screenSize;
+    properties (SetAccess=private)
+        screenSize; % screen resolution
     end
-    
-    
+
+
     %% public methods
     methods
-        % ======================================================================
-        %> @brief contructor
-        %> @param varargin como entrada puede recibir el numero del monitor en el que se quiere proyectar
-        % ======================================================================
-        function this=DisplayProjectorC(varargin)   
-            %mirar que varargin sea como maximo 1 parametro y usar por
-            %defecto 2
-            %verificar numero de proyectores y que el monitor pedido no
-            %exceda el numero de monitores
-            
+        function this=DisplayProjectorC(varargin)
+            % DisplayProjectorC constructs a C++-backed display
+            % projector. varargin{1}, if given, is the target monitor
+            % number (must not exceed the number of connected monitors);
+            % defaults to monitor 2 if there's more than one, else 1.
+
             posAll=get(0,'MonitorPositions');
             NMons=size(posAll, 1); %number of screen
             if NMons==1
@@ -65,28 +51,21 @@ classdef DisplayProjectorC <  handle
             this.Init();
         end                
         
-        % ======================================================================
-        %> @brief this funcion display a image in the screen
-        %> @param n number of the image in gList
-        % ======================================================================
         function Display(this, n)
+            % Display shows image n from gList on the target monitor
             calllib(this.Lib,'display', n);
         end
-        
-        % ======================================================================
-        %> @brief this funcion project one color in the screen
-        %> @param RGB matriz RGB (componentes entre 0 y 1) que identifica el color que se desea proyectar
-        % ======================================================================
+
         function DisplayRGB(this, RGB)
-            % Color the screen grey
+            % DisplayRGB fills the whole screen with solid color RGB
+            % (3-element vector, components in [0, 1])
             RGB = uint8(255*RGB);
             calllib(this.Lib,'displayRGB', RGB(1), RGB(2), RGB(3));
         end
-        
-        % ======================================================================
-        %> @brief this funcion pre charge the images before the display
-        % ======================================================================
+
         function TextureImages(this)
+            % TextureImages uploads every image in gList to the DLL
+            % (split into R/G/B planes) ahead of display, for faster Display
             calllib(this.Lib,'ImageMemo',length(this.gList));
             for n=1:length(this.gList)
                 %Preparar la imagen en sus tres componentes para enviar
@@ -105,20 +84,22 @@ classdef DisplayProjectorC <  handle
             end
         end
         
-        % ======================================================================
-        %> @brief this funcion clear the screen
-        % ======================================================================
         function CloseScreen(this)
+            % CloseScreen blanks the screen, releases the C++ projector
+            % object and unloads the CProjector library
             this.DisplayRGB([0 0 0]);
             calllib(this.Lib,'dele');
             unloadlibrary CProjector;
         end
     end
-    
-    
+
+
     %% private methods
     methods (Access=private)
         function this=Init(this)
+            % Init (re)loads the CProjector library, initializes it on
+            % this.Monitor, reads the resulting screen resolution, and
+            % blanks the screen
             this.Lib='CProjector';
             this.LibH=[this.Lib, '.h']; 
             
