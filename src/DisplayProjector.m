0e444b9c258b59cf43bfbe2f97efb3ff86d0413d
juaquiro <aq@iot.es>
2026-09-16 17:10:43 -0400

    Add docstrings to DeployPaths, DisplayFactory, DisplayProjector (GH #3)
    DisplayFactory/DisplayProjector converted from Doxygen-style (%>)
    comments to plain MATLAB help text, DeployPaths translated from
    Spanish. Comment-only changes, verified with check_matlab_code.

--- src/DisplayProjector.m
+++ src/DisplayProjector.m
@@ -1,54 +1,40 @@
-%> @file DisplayProjector.m
-%> @brief Java wrapper image projector class
-%> @copyright 2019 IOT
-
-% ======================================================================
-%> @brief this class implements the Java wrapper image projector class
-%> @brief this class uses physical display size, if there is scaling this
-%> @brief size is different from logical display size. 
-%> @brief if logical size is needed use DisplayProjectorMatlab.
-%> @brief see DisplayProjectorMatlab.Init()
-%> @see DisplayTypes for avalible projector follower types
-%> @see DisplayFactory for a static factory
-%> @see testFPADisplayProjector for unit tests
-% ======================================================================
 classdef DisplayProjector <  handle
-    %% props    
+    % DisplayProjector Java-wrapper (fullscreen.m) image projector
+    %
+    % Description:
+    %   Uses the physical display size - if the OS applies scaling, this
+    %   differs from the logical display size; use DisplayProjectorMatlab
+    %   instead if logical size is needed (see its Init()). See
+    %   DisplayTypes for available projector types, DisplayFactory for a
+    %   static factory, and testFPADisplayProjector for unit tests.
+    %% props
     %public
-    properties  
-        %> cell list of images  
-        gList; 
-        %> number of the screen
-        Monitor;
+    properties
+        gList; % cell list of images
+        Monitor; % number of the screen
         flipMonitor; %default true
     end
-            
+
     %get only propos
-    properties (SetAccess=private)      
-        %> screen resolution
-        screenSize;
+    properties (SetAccess=private)
+        screenSize; % screen resolution
     end
-    
-    
+
+
     %% public methods
     methods
-        % ======================================================================
-        %> @brief constructor
-        % ======================================================================
-        function this=DisplayProjector()   
-            %interchange monitor 0->1     
-            %AQ 5MAY20 esto es una ñapa para salir del paso con win7 en PC
+        function this=DisplayProjector()
+            % DisplayProjector constructs a Java-wrapper display projector
+            %interchange monitor 0->1
+            %AQ 5MAY20 esto es una ï¿½apa para salir del paso con win7 en PC
             %IOT Mapper en espera de actualizar a win10
             this.flipMonitor=true;
             % Props Initialization
             this.Init();
-        end                
-        
-        % ======================================================================
-        %> @brief this funcion display a image in the screen
-        %> @param n number of the image in gList
-        % ======================================================================
+        end
+
         function Display(this, n)
+            % Display shows image n from gList on the target monitor
             import OM4MClassLib.Util.*;
             callFunc=Logging.WhoCalledMe();
             
@@ -62,12 +48,9 @@ classdef DisplayProjector <  handle
             fullscreen(this.gList{n}, this.Monitor);    
         end
         
-        % ======================================================================
-        %> @brief this funcion project one color in the screen
-        %> @param RGB matriz RGB (componentes entre 0 y 1) que identifica el color que se desea proyectar
-        % ======================================================================
         function DisplayRGB(this, RGB)
-            % Color the screen grey
+            % DisplayRGB fills the whole screen with solid color RGB
+            % (3-element vector, components in [0, 1])
             X(:,:,1) = uint8(255*RGB(1)*ones(this.screenSize(1),this.screenSize(2)));
             X(:,:,2) = uint8(255*RGB(2)*ones(this.screenSize(1),this.screenSize(2)));
             X(:,:,3) = uint8(255*RGB(3)*ones(this.screenSize(1),this.screenSize(2)));
@@ -75,27 +58,24 @@ classdef DisplayProjector <  handle
             fullscreen(X, this.Monitor);  
         end
         
-        % ======================================================================
-        %> @brief this funcion pre charge the images before the display in others projectors
-        % ======================================================================
         function TextureImages(this)
-            
+            % TextureImages no-op here (other projectors pre-load
+            % images before display; this one doesn't need to)
+
         end
-        
-        % ======================================================================
-        %> @brief this funcion clear the screen
-        % ======================================================================
+
         function CloseScreen(this)
+            % CloseScreen closes the fullscreen window
             closescreen;
         end
     end
-    
-    
+
+
     %% private methods
     methods (Access=private)
         function this=Init(this)
-
-            
+            % Init detects the target monitor (flipping 0<->1 on 2-monitor
+            % setups if flipMonitor) and reads its physical resolution
             %pillamos posicion de los monitores disponibles
             posAll=get(0,'MonitorPositions');
             
